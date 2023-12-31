93d8f3deace9225690105daede21ad76117b0564
Pankaj Agham <ina809panagha@ext.crifnet.com>
2023-12-31 19:06:58 +0530

    login form component UI style added

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,10 +1,26 @@
+import { RxCross2 } from "react-icons/rx";
+import {Link} from "react-router-dom";
+import "./style.css";
 
-import "./style.css"
 const Login = () => {
     return (
-        <>
-            Login
-        </>
+        <div className="login">
+            <div className="login-form-container">
+                <div className="login-form-header">
+                    <h3 className="form-title">Log in</h3>
+                    <RxCross2 className="cross-icon"/>
+                </div>
+                <div className="login-form-content">
+                    <label className="form-item" htmlFor="username">Username</label>
+                    <input className="form-item username-input" type="text" placeholder="Username"/>
+                    <label className="form-item" htmlFor="password">Password</label>
+                    <input className="form-item" type="text" placeholder="Password" />
+                    <div className="form-login-btn">Login</div>
+                    <p>Not a user? 
+                        <span><Link to={"/"} className="register-link">Register here</Link></span></p>
+                </div>
+            </div>
+        </div>
     )
 }
 
